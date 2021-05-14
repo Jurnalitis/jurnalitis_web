c63eb87fdea3f2f7a4e58d362abca58beff0ea22
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2021-05-14 22:57:03 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# jurnalitis_web
+
+Kode sumber website jurnalitis.com. Dibuat oleh Flutter
+  
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:jurnalitis_web/ui/view/homepage_view.dart';
+
+void main() {
+  runApp(Jurnalitis());
+}
+
+class Jurnalitis extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Jurnalitis',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: HomePageView(),
+    );
+  }
+}
