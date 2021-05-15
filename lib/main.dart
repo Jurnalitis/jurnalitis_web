817e507c13fe5683ff3c4382a7ee948ceadd711c
Bagas Wastu <67826350+bagaswastu@users.noreply.github.com>
2021-05-15 12:50:12 +0700

    Membuat website menjadi lebih responsif

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:jurnalitis_web/ui/view/homepage_view.dart';
+import 'package:responsive_framework/responsive_wrapper.dart';
 
 void main() {
   runApp(Jurnalitis());
@@ -9,13 +10,23 @@ class Jurnalitis extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      builder: (context, widget) => ResponsiveWrapper.builder(
+          HomePageView(),
+          maxWidth: 1200,
+          minWidth: 480,
+          defaultScale: true,
+          breakpoints: [
+            ResponsiveBreakpoint.resize(480, name: MOBILE),
+            ResponsiveBreakpoint.autoScale(800, name: TABLET),
+            ResponsiveBreakpoint.resize(1000, name: DESKTOP),
+          ],
+          background: Container(color: Color(0xFFF5F5F5))),
       title: 'Jurnalitis',
       theme: ThemeData(
         primarySwatch: Colors.blueGrey,
         hoverColor: Colors.transparent,
         highlightColor: Colors.transparent,
       ),
-      home: HomePageView(),
     );
   }
 }
